942f3b19e34426a88e77d59b96bce33fcf4ae7fc
liliana <48772602+lilaorlova@users.noreply.github.com>
2023-11-05 23:56:20 +0300

    Update Train.py

--- Homework_model_wandb/Train.py
+++ Homework_model_wandb/Train.py
@@ -2,6 +2,7 @@ from torchvision import transforms
 from torchvision.datasets import CIFAR10, MNIST
 import os
 
+from lightning.pytorch.loggers import WandbLogger
 import lightning as L
 import torch
 import torchvision
@@ -10,6 +11,8 @@ from torch import nn
 from torch.utils.data import DataLoader, random_split
 from torchmetrics.functional import accuracy
 from torchvision import transforms
+from CIFAR10DataModule import CIFAR10DataModule
+from CIFAR10Model import CIFAR10Model
 
 dm = CIFAR10DataModule()
 
@@ -20,6 +23,7 @@ trainer = L.Trainer(
     max_epochs=1,
     accelerator="auto",
     devices=1,
+    logger=wandb_logger,
 )
 trainer.fit(model, dm)
 trainer.test()
